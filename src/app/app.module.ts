9fadb2e063f0feb45407d359b30e19b4095e7019
fercadec <fercajomo9@gmail.com>
2020-11-16 18:04:32 -0500

    complete page ngIf and ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { Body3Component } from './components/body3/body3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    Body3Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,20 @@
+import { from } from 'rxjs';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent{
+
+    mostrar = true;
+
+frase: any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+};
+
+personajes: string[] = ['Iroman', 'Spidermnan', 'Batman','Thor']
+
+}
